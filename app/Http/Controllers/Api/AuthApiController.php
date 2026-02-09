7dd6bf010dcb7451739f140b0c7f0be73e109c92
mediaclockdev <mediaclockdev@gmail.com>
2026-02-09 15:24:00 +1100

    updated code

--- app/Http/Controllers/Api/AuthApiController.php
+++ app/Http/Controllers/Api/AuthApiController.php
@@ -180,7 +180,7 @@ class AuthApiController extends Controller
 
         // Create API token
         $user->tokens()->delete();
-        $token = $user->createToken('api')->plainTextToken;
+        $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
             'status'  => true,

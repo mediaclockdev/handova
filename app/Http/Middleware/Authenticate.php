01c2e48ce9e479e3bfb517870af98f71d839959e
mediaclockdev <mediaclockdev@gmail.com>
2026-02-09 16:05:03 +1100

    latest code

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -13,11 +13,13 @@ class Authenticate extends Middleware
     protected function redirectTo(Request $request): ?string
     {
         if (! $request->expectsJson()) {
-
-        if ($request->is('superadmin/*')) {
+            if ($request->is('superadmin/*')) {
                 return route('superadmin.login');
             }
+
             return route('login');
         }
+
+        return null; // ✅ REQUIRED
     }
 }
